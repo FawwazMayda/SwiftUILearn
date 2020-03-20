d2252c50257c4df16d1143f442d7d6ffa1b41149
fawwazmayda <maydafawwaz@gmail.com>
2020-03-20 21:45:47 +0700

    add some assets

--- HandlingUserInput/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
+++ HandlingUserInput/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -22,7 +22,7 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+        ForEach(["iPhone 8 Plus", "iPhone XR"], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
