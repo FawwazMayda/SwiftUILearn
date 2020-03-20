5464bf5245279ac679bfc4da681d056451406a53
fawwazmayda <maydafawwaz@gmail.com>
2020-03-20 16:04:17 +0700

    adding CircularImage.swift

---
+++ SwiftUIEssential/SwiftUIEssential/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  SwiftUIEssential
+//
+//  Created by Muhammad Fawwaz Mayda on 20/03/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
