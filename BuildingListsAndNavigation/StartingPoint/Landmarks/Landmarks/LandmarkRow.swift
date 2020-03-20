3800529617410850f6a52d4e8a15de842f49007c
fawwazmayda <maydafawwaz@gmail.com>
2020-03-20 19:10:49 +0700

    Hstack and Image

--- BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkRow.swift
+++ BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkRow.swift
@@ -12,7 +12,10 @@ struct LandmarkRow: View {
     var landmark : Landmark
     var body: some View {
         HStack{
+            landmark.image
+            .resizable().frame(width:30, height:30)
             Text(landmark.name)
+            Spacer()
         }
     }
 }
