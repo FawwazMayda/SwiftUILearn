7bbc30bd6ad3dc9396ad53eadad525ae2b3b6eb9
fawwazmayda <maydafawwaz@gmail.com>
2020-03-20 21:21:02 +0700

    adjusting landmarkdetail.swift

--- BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkDetail.swift
+++ BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkDetail.swift
@@ -11,22 +11,22 @@ struct LandmarkDetail: View {
     var landmark : Landmark
     var body: some View {
         VStack {
-            MapView()
+            MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
 
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
-                Text("Turtle Rock")
+                Text(landmark.name)
                     .font(.title)
                 HStack(alignment: .top) {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                         .font(.subheadline)
                     Spacer()
-                    Text("California")
+                    Text(landmark.state)
                         .font(.subheadline)
                 }
             }
@@ -39,6 +39,6 @@ struct LandmarkDetail: View {
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarkData[1])
     }
 }
