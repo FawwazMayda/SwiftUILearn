a8babcebb23ac3251e83445de43b9ae5bbcf42b2
fawwazmayda <maydafawwaz@gmail.com>
2020-03-21 22:07:56 +0700

    add Hexagon Parameters to Badge.swift

--- DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/Badge.swift
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/Badge.swift
@@ -12,6 +12,18 @@ struct Badge: View {
     var body: some View {
         Path {
             path in
+            let height : CGFloat = 100.0
+            let width = height
+            path.move(to: CGPoint(x: 0.95*width, y: 0.20*height))
+            
+            HexagonParameters.points.forEach {
+                path.addLine(
+                    to: .init(
+                        x: width * $0.useWidth.0 * $0.xFactors.0,
+                        y: height * $0.useHeight.0 * $0.yFactors.0
+                    )
+                )
+            }
             
         }
         .fill(Color.black)
