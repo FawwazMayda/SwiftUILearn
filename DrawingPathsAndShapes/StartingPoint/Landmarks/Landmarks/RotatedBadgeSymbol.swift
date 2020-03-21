85098a9699b15206eadd63fbab4d0d0e4585d05d
fawwazmayda <maydafawwaz@gmail.com>
2020-03-21 22:34:06 +0700

    adding RotatedBadgeSymbol.swift

---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Muhammad Fawwaz Mayda on 21/03/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle : Angle
+    var body: some View {
+        BadgeSymbol()
+        .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle : .init(degrees: 5.0))
+    }
+}
