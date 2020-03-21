6610992b91d10ffcea29a3f5e2ffb0b040feebd3
fawwazmayda <maydafawwaz@gmail.com>
2020-03-21 22:27:34 +0700

    adding some boiletplate code

---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/BadgeSymbol.swift
@@ -0,0 +1,38 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Muhammad Fawwaz Mayda on 21/03/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width / 2
+                let topWidth = 0.226 * width
+                let topHeight = 0.488 * height
+                
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                ])
+            }
+        }
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
