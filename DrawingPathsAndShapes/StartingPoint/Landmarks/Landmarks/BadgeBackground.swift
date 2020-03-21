5d892d220d9daf6048374ecb8da242f60969aaec
fawwazmayda <maydafawwaz@gmail.com>
2020-03-21 22:23:19 +0700

    making a badgebackground.swift

---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/BadgeBackground.swift
@@ -0,0 +1,21 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Muhammad Fawwaz Mayda on 21/03/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
