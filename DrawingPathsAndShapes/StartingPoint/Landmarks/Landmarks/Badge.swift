6643df2a79434384fa6571b174a6588fff5d3f21
fawwazmayda <maydafawwaz@gmail.com>
2020-03-21 22:19:40 +0700

    aspect Ratio and Content Fit

--- DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/Badge.swift
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/Badge.swift
@@ -44,7 +44,7 @@ struct Badge: View {
                 gradient: .init(colors: [Self.gradientStart, Self.gradientEnd]),
                 startPoint: .init(x: 0.5, y: 0),
                 endPoint: .init(x: 0.5, y: 0.6)
-            ))
+            )).aspectRatio(1.0, contentMode: .fit)
         }
     }
     static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
