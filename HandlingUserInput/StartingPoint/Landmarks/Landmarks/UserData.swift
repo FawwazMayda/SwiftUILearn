76126a5d20030aae6d79fc38c233bfb17eeb4cab
fawwazmayda <maydafawwaz@gmail.com>
2020-03-21 18:41:09 +0700

    making UserData.swift

---
+++ HandlingUserInput/StartingPoint/Landmarks/Landmarks/UserData.swift
@@ -0,0 +1,16 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Muhammad Fawwaz Mayda on 21/03/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    var showFavouritesOnly = false
+    var landmark = landmarkData
+    
+}
