5d1a5a4e18282cf1d26cdb9d57f5f0a8cb05899f
fawwazmayda <maydafawwaz@gmail.com>
2020-03-21 22:14:58 +0700

    adding some Geomtery

--- DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/Badge.swift
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/Badge.swift
@@ -10,31 +10,35 @@ import SwiftUI
 
 struct Badge: View {
     var body: some View {
-        Path {
-            path in
-            let height : CGFloat = 100.0
-            let width = height
-            path.move(to: CGPoint(x: 0.95*width, y: 0.20*height))
-            
-            HexagonParameters.points.forEach {
-                path.addLine(
-                    to: .init(
-                        x: width * $0.useWidth.0 * $0.xFactors.0,
-                        y: height * $0.useHeight.0 * $0.yFactors.0
-                    )
-                )
+        GeometryReader { geometry in
+            Path {
+                path in
+                let width : CGFloat = min(geometry.size.height,geometry.size.width)
+                let height = width
+                path.move(to:
+                    CGPoint(x: 0.95*width,
+                            y: height * (HexagonParameters.adjustment + 0.20)))
                 
-                path.addQuadCurve(
-                    to: .init(
-                        x: width * $0.useWidth.1 * $0.xFactors.1,
-                        y: height * $0.useHeight.1 * $0.yFactors.1)
-                    , control: .init(
-                        x: width * $0.useWidth.2 * $0.xFactors.2,
-                        y: height * $0.useHeight.2 * $0.yFactors.2))
+                HexagonParameters.points.forEach {
+                    path.addLine(
+                        to: .init(
+                            x: width * $0.useWidth.0 * $0.xFactors.0,
+                            y: height * $0.useHeight.0 * $0.yFactors.0
+                        )
+                    )
+                    
+                    path.addQuadCurve(
+                        to: .init(
+                            x: width * $0.useWidth.1 * $0.xFactors.1,
+                            y: height * $0.useHeight.1 * $0.yFactors.1)
+                        , control: .init(
+                            x: width * $0.useWidth.2 * $0.xFactors.2,
+                            y: height * $0.useHeight.2 * $0.yFactors.2))
+                }
+                
             }
-            
+            .fill(Color.black)
         }
-        .fill(Color.black)
     }
 }
 
