acd1edb731c804705a297f3c78faf8a6a21cc458
fawwazmayda <maydafawwaz@gmail.com>
2020-03-21 22:02:38 +0700

    adding Badge Swift

---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Muhammad Fawwaz Mayda on 21/03/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("BAdge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
