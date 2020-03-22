38bee093dcbd8f240438727a2cfaa1f03e9de48c
fawwazmayda <maydafawwaz@gmail.com>
2020-03-22 18:44:38 +0700

    just follow them

--- AnimatingViewsAndTransitions/StartingPoint/Landmarks/Landmarks/Supporting Views/HikeGraph.swift
+++ AnimatingViewsAndTransitions/StartingPoint/Landmarks/Landmarks/Supporting Views/HikeGraph.swift
@@ -19,6 +19,13 @@ func magnitude(of range: Range<Double>) -> Double {
     return range.upperBound - range.lowerBound
 }
 
+extension Animation {
+    static func ripple(index: Int) -> Animation {
+        return Animation.spring(dampingFraction: 0.5)
+        .speed(2)
+            .delay(0.03 * Double(index))
+    }
+}
 struct HikeGraph: View {
     var hike: Hike
     var path: KeyPath<Hike.Observation, Range<Double>>
