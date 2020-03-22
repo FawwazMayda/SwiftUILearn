4955ecaaedc18b1d676b9ea6288475521daa7cda
fawwazmayda <maydafawwaz@gmail.com>
2020-03-22 22:00:14 +0700

    Home Swift

---
+++ ComposingComplexInterfaces 2/StartingPoint/Landmarks/Landmarks/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  Landmarks
+//
+//  Created by Muhammad Fawwaz Mayda on 22/03/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
