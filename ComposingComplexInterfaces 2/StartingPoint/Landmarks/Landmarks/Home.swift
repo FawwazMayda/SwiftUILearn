91f2c72d9dc11b33f3bd0cc38e3f61bc4f158241
fawwazmayda <maydafawwaz@gmail.com>
2020-03-22 22:06:10 +0700

    add some Boilerplate

--- ComposingComplexInterfaces 2/StartingPoint/Landmarks/Landmarks/Home.swift
+++ ComposingComplexInterfaces 2/StartingPoint/Landmarks/Landmarks/Home.swift
@@ -9,6 +9,14 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    
+    var categories : [String: [Landmark]] {
+        Dictionary(
+        grouping: landmarkData,
+        by: {$0.category.rawValue}
+        )
+    }
+    
     var body: some View {
         NavigationView {
             Text("Landmark Home")
